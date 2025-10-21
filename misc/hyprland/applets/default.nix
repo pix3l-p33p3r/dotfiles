ebd030dfaf8cb06bc21454bf4b02759f0a18cf4c
El houcine  El yakoubi <elel-yak@c4r5p3.1337.ma>
2025-10-21 06:16:59 +0100

    Temporarily disable sherlock modules due to fork change
    - Commented out sherlock homeModules import in default.nix
    - Commented out sherlock.nix import in applets/default.nix
    - The new Skxxtz fork may have different module structure
    - Sherlock package is still available via variables.nix for keybindings

--- misc/hyprland/applets/default.nix
+++ misc/hyprland/applets/default.nix
@@ -4,7 +4,7 @@
     ./waybar.nix
     ./sway-notification-center.nix
     ./wofi.nix
-    ./sherlock.nix
+    # ./sherlock.nix  # Temporarily disabled due to module structure change
     ./swww.nix
   ];
 }
