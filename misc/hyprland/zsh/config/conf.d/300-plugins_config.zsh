b240050cc492c1900f65bb1653cafa1770a512f2
pixel-peeper <pix3l-p33p3r@proton.me>
2025-10-21 20:00:05 +0100

    Fix waybar duplication and add power profile module
    - Fixed waybar component duplication issue
    - Added power profile module with power-saver, balanced, and performance modes
    - Added power-profiles-daemon package dependency
    - Fixed JSON parsing error for power profile module
    - Added color-coded styling for different power modes

--- misc/hyprland/zsh/config/conf.d/300-plugins_config.zsh
+++ misc/hyprland/zsh/config/conf.d/300-plugins_config.zsh
@@ -1,4 +1,4 @@
-# "agkozak/agkozak-zsh-prompt" - Catppuccin Mocha theme
+# "agkozak/agkozak-zsh-prompt"
 AGKOZAK_MULTILINE=0
 AGKOZAK_LEFT_PROMPT_ONLY=1
 AGKOZAK_PROMPT_DIRTRIM=2
@@ -6,26 +6,22 @@ AGKOZAK_BLANK_LINES=1
 AGKOZAK_SHOW_STASH=1
 AGKOZAK_SHOW_BG=1
 AGKOZAK_BG_STRING='j'
-
-# Catppuccin Mocha colors
-AGKOZAK_COLORS_EXIT_STATUS='%F{#f38ba8}'      # red
-AGKOZAK_COLORS_USER_HOST='%F{#a6e3a1}'        # green  
-AGKOZAK_COLORS_PATH='%F{#89b4fa}'             # blue
-AGKOZAK_COLORS_BRANCH_STATUS='%F{#f9e2af}'    # yellow
-AGKOZAK_COLORS_PROMPT_CHAR='%F{#cdd6f4}'      # text (white/light)
-AGKOZAK_COLORS_CMD_EXEC_TIME='%F{#6c7086}'    # overlay0 (gray)
-AGKOZAK_COLORS_VIRTUALENV='%F{#a6e3a1}'       # green
-AGKOZAK_COLORS_BG_STRING='%F{#cba6f7}'        # mauve
+AGKOZAK_COLORS_EXIT_STATUS=red
+AGKOZAK_COLORS_USER_HOST=green
+AGKOZAK_COLORS_PATH=blue
+AGKOZAK_COLORS_BRANCH_STATUS=yellow
+AGKOZAK_COLORS_PROMPT_CHAR=white      # Default text color
+AGKOZAK_COLORS_CMD_EXEC_TIME=gray    # Default text color
+AGKOZAK_COLORS_VIRTUALENV=green
+AGKOZAK_COLORS_BG_STRING=magenta
 
 
-# "zsh-users/zsh-history-substring-search" - Catppuccin Mocha colors
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=transparent,fg=#a6e3a1,bold'      # green
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=transparent,fg=#f38ba8,bold'   # red
+# "zsh-users/zsh-history-substring-search"
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=transparent,fg=green,bold'
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=transparent,fg=red,bold'
 HISTORY_SUBSTRING_SEARCH_FUZZY=1
 HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
 HISTORY_SUBSTRING_SEARCH_PREFIXED=1
 
-# "zsh-users/zsh-autosuggestions" - Catppuccin Mocha colors
+# "zsh-users/zsh-autosuggestions"
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#6c7086'  # overlay0 (muted gray for suggestions)
-
