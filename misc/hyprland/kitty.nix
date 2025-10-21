bdaf4a6a78557a642ce5fcd028cf6be7f1dad256
El houcine  El yakoubi <elel-yak@c4r5p3.1337.ma>
2025-10-21 05:45:47 +0100

    Use catppuccin home-manager module for kitty theming instead of manual colors

--- misc/hyprland/kitty.nix
+++ misc/hyprland/kitty.nix
@@ -54,57 +54,6 @@
       shell_integration = "enabled";
       allow_cloning = "ask";
       notify_on_cmd_finish = "unfocused 10.0 bell";
-
-      # Catppuccin Mocha Theme Colors
-      foreground = "#CDD6F4";
-      background = "#1E1E2E";
-      selection_foreground = "#1E1E2E";
-      selection_background = "#F5E0DC";
-
-      # Cursor colors
-      cursor = "#F5E0DC";
-      cursor_text_color = "#1E1E2E";
-
-      # URL underline color
-      url_color = "#F5E0DC";
-
-      # Window border colors
-      active_border_color = "#B4BEFE";
-      inactive_border_color = "#6C7086";
-      bell_border_color = "#F9E2AF";
-
-      # Tab bar colors
-      active_tab_foreground = "#11111B";
-      active_tab_background = "#CBA6F7";
-      inactive_tab_foreground = "#CDD6F4";
-      inactive_tab_background = "#181825";
-      tab_bar_background = "#11111B";
-
-      # Mark colors
-      mark1_foreground = "#1E1E2E";
-      mark1_background = "#B4BEFE";
-      mark2_foreground = "#1E1E2E";
-      mark2_background = "#CBA6F7";
-      mark3_foreground = "#1E1E2E";
-      mark3_background = "#74C7EC";
-
-      # Terminal colors
-      color0 = "#45475A";
-      color1 = "#F38BA8";
-      color2 = "#A6E3A1";
-      color3 = "#F9E2AF";
-      color4 = "#89B4FA";
-      color5 = "#F5C2E7";
-      color6 = "#94E2D5";
-      color7 = "#BAC2DE";
-      color8 = "#585B70";
-      color9 = "#F38BA8";
-      color10 = "#A6E3A1";
-      color11 = "#F9E2AF";
-      color12 = "#89B4FA";
-      color13 = "#F5C2E7";
-      color14 = "#94E2D5";
-      color15 = "#A6ADC8";
     };
     extraConfig = ''
       # Symbol mapping for powerline
