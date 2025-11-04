75948fcd8bce83694f626d371f378449221e8762
pixel-peeper <pix3l-p33p3r@proton.me>
2025-11-04 15:23:14 +0100

    cli: add timewarrior+tmux; enable tmux with Catppuccin; add task/timew aliases; minimal task/timew configs

--- homes/pixel-peeper/default.nix
+++ homes/pixel-peeper/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ../../configs/desktop/hyprland
     ../../configs/editors/cursor-config.nix
+    ../../configs/terminal/tmux.nix
     ./catppuccin.nix
     ./sops.nix
   ];
@@ -36,6 +37,38 @@
   home.packages = [
     (pkgs.callPackage ../../configs/editors/cursor.nix {})
   ];
+
+  # Minimal themed configs for Taskwarrior / Timewarrior
+  home.file = {
+    ".taskrc".text = ''
+      # Taskwarrior basic settings
+      confirmation=no
+      dateformat=y-M-d
+      weekstart=Monday
+      color=on
+      # Catppuccin-ish colors
+      color.overdue=bold red
+      color.due=yellow
+      color.blocking=bright cyan
+      color.tagged=magenta
+      color.pri.H=bold bright red
+      color.pri.M=bold yellow
+      color.pri.L=bold green
+    '';
+
+    ".timewarrior/config".text = ''
+      # Timewarrior configuration
+      verbose = yes
+      month.letters = 3
+      confirmation = no
+      colors = on
+      theme.colors = 256
+      reports.day.range = day
+      reports.week.range = week
+      reports.month.range = month
+      summary.ids = off
+    '';
+  };
 
   home.username = "pixel-peeper";
   home.homeDirectory = "/home/pixel-peeper";
