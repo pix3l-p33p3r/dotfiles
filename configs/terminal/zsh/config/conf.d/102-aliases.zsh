bc486d7ff0c3980757ba253520015f95d1d81671
pixel-peeper <pix3l-p33p3r@proton.me>
2025-11-04 06:47:42 +0100

    zsh alias for making build time faster

--- configs/terminal/zsh/config/conf.d/102-aliases.zsh
+++ configs/terminal/zsh/config/conf.d/102-aliases.zsh
@@ -35,10 +35,10 @@ alias clean="$HOME/dotfiles/scripts/nix-cleaner.sh"
 alias update="nix flake update && fastfetch"
 
 # NixOS system rebuild
-alias nrs="sudo nixos-rebuild switch --flake '$HOME/dotfiles#alucard' -j 7 --no-reexec"
+alias nrs="sudo nixos-rebuild switch --flake '$HOME/dotfiles#alucard' --fast --no-reexec"
 
-# Home Manager rebuild
-alias hms="home-manager switch --flake '$HOME/dotfiles#pixel-peeper@alucard'"
+# Home Manager rebuild (optimized for speed)
+alias hms="home-manager switch --flake '$HOME/dotfiles#pixel-peeper@alucard' --fast"
 
 # Full upgrade: update flake, rebuild system and home-manager, then clean
 alias upgrade="cd $HOME/dotfiles && nix flake update && nrs && hms && clean && fastfetch"
