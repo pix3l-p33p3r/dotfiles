3473fe108815b38b80a5073cc7c4c11bc79776de
pix3l-p33p3r <pix3l-p33p3r@users.noreply.github.com>
2026-03-09 11:38:45 +0000

    tweaking some stuff

--- machines/alucard/virt.nix
+++ machines/alucard/virt.nix
@@ -9,8 +9,9 @@
   systemd.services.libvirt-guests.wantedBy = lib.mkForce []; # Disable guest shutdown
 
   # Upstream unit uses /usr/bin/sh, which doesn't exist on NixOS.
-  # Force a Nix-safe ExecStart with runtimeShell and explicit binary paths.
-  systemd.services.virt-secret-init-encryption.serviceConfig.ExecStart = lib.mkForce ''
-    ${pkgs.runtimeShell} -c 'umask 0077 && (${pkgs.coreutils}/bin/dd if=/dev/random status=none bs=32 count=1 | ${pkgs.systemd}/bin/systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'
-  '';
+  # Reset ExecStart then set a Nix-safe command.
+  systemd.services."virt-secret-init-encryption".serviceConfig.ExecStart = lib.mkForce [
+    ""
+    "${pkgs.runtimeShell} -c 'umask 0077 && (${pkgs.coreutils}/bin/dd if=/dev/random status=none bs=32 count=1 | ${pkgs.systemd}/bin/systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'"
+  ];
 }
